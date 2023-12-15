a36bb0f34a44b46aac5e354c247cf5ce0a4c909b
adsonb87 <adsonb87@gmail.com>
2023-12-15 18:11:09 -0300

    BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
